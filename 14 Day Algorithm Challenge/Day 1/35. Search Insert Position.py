8cb1a2b3a5ea90ab1749548b098eb6322e17c63f
isalia20 <irakli.salia854@gmail.com>
2022-07-06 21:00:24 +0400

    Added 35th problem Search and Invert Position

---
+++ 14 Day Algorithm Challenge/Day 1/35. Search Insert Position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            pivot = int((left + right) / 2)
+            if nums[pivot] == target:
+                return pivot
+
+            if nums[pivot] > target:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return left
