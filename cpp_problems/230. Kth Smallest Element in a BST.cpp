20f86481a52a4b8604c38b6cb97bfaa7fb5ee90d
Isalia20 <65120973+Isalia20@users.noreply.github.com>
2023-02-24 23:43:09 +0400

    Kth smallest element with less memory consumption

--- cpp_problems/230. Kth Smallest Element in a BST.cpp
+++ cpp_problems/230. Kth Smallest Element in a BST.cpp
@@ -10,23 +10,31 @@
  * };
  */
 class Solution {
-    vector<int> result;
+    int result;
+    int counter = 0;
+    bool is_set;
 
 public:
-    int traverseTree(TreeNode* root){
+    int traverseTree(TreeNode* root, int k){
         if (root == NULL){
             return 0;
         }
 
-        traverseTree(root -> left);
-        result.push_back(root -> val);
-        traverseTree(root -> right);
+        traverseTree(root -> left, k);
+        // takes less memory
+        if (counter == k - 1 and not is_set){
+            result = root -> val;
+            is_set = true;
+        } else {
+            counter++;
+        }
+        traverseTree(root -> right, k);
         
         return 0;
     }
 
     int kthSmallest(TreeNode* root, int k) {
-        traverseTree(root);
-        return result[k - 1];
+        traverseTree(root, k);
+        return result;
     }
 };
