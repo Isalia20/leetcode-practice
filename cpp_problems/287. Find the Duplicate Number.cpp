0c76974dde7b6795f059107b2a04f47f3ceb5226
Isalia20 <65120973+Isalia20@users.noreply.github.com>
2023-03-05 21:48:39 +0400

    update to old bad solution

--- cpp_problems/287. Find the Duplicate Number.cpp
+++ cpp_problems/287. Find the Duplicate Number.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++){
-            if (nums[i] == nums[i - 1]){
-                return nums[i];
+        int slow = 0;
+        int fast = 0;
+        while (true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast){
+                break;
             }
         }
-        return 0;
+
+        int slow_2 = 0;
+        while (slow_2 != slow){
+            slow_2 = nums[slow_2];
+            slow = nums[slow];
+        }
+        return slow;
     }
 };
